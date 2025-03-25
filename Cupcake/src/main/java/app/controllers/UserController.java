ac1fb78860a9866d3641b2526ee3e828c972ccbb
NicolaiKristiansen <nicolaikristiansen223@gmail.com>
2025-03-25 11:44:13 +0100

    Did alot

--- Cupcake/src/main/java/app/controllers/UserController.java
+++ Cupcake/src/main/java/app/controllers/UserController.java
@@ -1,10 +1,8 @@
 package app.controllers;
 
-import app.entities.Task;
 import app.entities.User;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
-import app.persistence.TaskMapper;
 import app.persistence.UserMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
@@ -22,14 +20,14 @@ public class UserController {
 
     private static void createUser(Context ctx, ConnectionPool connectionPool) {
 // Hent form parametre
-        String username = ctx.formParam("username");
+        String email = ctx.formParam("email");
         String password1 = ctx.formParam("password1");
         String password2 = ctx.formParam("password2");
 
         if (password1.equals(password2)) {
             try {
-                UserMapper.createuser(username, password1, connectionPool);
-                ctx.attribute("message", "Du er hermed oprettet med brugernavn" + username + "." +
+                UserMapper.createuser(email, password1, connectionPool);
+                ctx.attribute("message", "Du er hermed oprettet med brugernavn" + email + "." +
                         " Nu skal du logge på.");
                 ctx.render("index.html");
             } catch (DatabaseException e) {
@@ -49,26 +47,20 @@ public class UserController {
 
     public static void login(Context ctx, ConnectionPool connectionPool) {
         // Hent form parametre
-        String username = ctx.formParam("username");
+        String email = ctx.formParam("email");
         String password = ctx.formParam("password");
 
         // Check om bruger findes i databasen med de angivende username + password
         try {
-            User user = UserMapper.login(username, password, connectionPool);
+            User user = UserMapper.login(email, password, connectionPool);
             ctx.sessionAttribute("currentUser", user);
             // Hvis ja, send videre til task side
-            List<Task> taskList = TaskMapper.getAllTasksPerUser(user.getUserId(), connectionPool);
-            ctx.attribute("taskList",  taskList);
+            //ctx.attribute("taskList",  taskList);
             ctx.render("task.html");
         }catch (DatabaseException e) {
             // Hvis nej, send tilbage til login side med fejl
             ctx.attribute("message", e.getMessage());
             ctx.render("index.html");
         }
-
-
-
-
-
     }
 }
