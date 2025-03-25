06a09af0a53ddda4d7faa24ff2cd6adea2ad72a4
sofusk7cdk <sofus@k7c.dk>
2025-03-25 10:03:35 +0100

    Project is setup

--- Cupcake/src/main/java/app/Main.java
+++ Cupcake/src/main/java/app/Main.java
@@ -1,8 +1,43 @@
 package app;
 
+import app.config.SessionConfig;
+import app.config.ThymeleafConfig;
+import app.controllers.TaskController;
+import app.controllers.UserController;
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+import java.util.logging.Logger;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // New setup branch
-        System.out.println("Hello, World!");
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "fourthingsplus";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+
+    public static void main(String[] args)
+    {
+        // Initializing Javalin and Jetty webserver
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.jetty.modifyServletContextHandler(handler ->  handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+        }).start(7070);
+
+        // Routing
+
+        app.get("/", ctx ->  ctx.render("index.html"));
+
+        UserController.addRoutes(app, connectionPool);
+        TaskController.addRoutes(app, connectionPool);
     }
+
 }
