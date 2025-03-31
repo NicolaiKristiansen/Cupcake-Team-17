282de39eaf456b780517239a7656337dfbc35b19
NicolaiKristiansen <nicolaikristiansen223@gmail.com>
2025-03-31 10:10:59 +0200

    - Removed some comments

--- Cupcake/src/main/java/app/controllers/RouteController.java
+++ Cupcake/src/main/java/app/controllers/RouteController.java
@@ -5,12 +5,10 @@ import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 public class RouteController {
-    //Class created by Nicolai
 
     private static CupcakeController cupcakeController = new CupcakeController();
 
     public static void routes(Javalin app, ConnectionPool connectionPool) {
-        //Function by Nicolai
         //When we go to a new path it goes through here
         app.get("/home", ctx -> homePage(ctx, connectionPool));
         app.get("/admin_index", ctx -> admin_index(ctx, connectionPool) );
@@ -18,7 +16,6 @@ public class RouteController {
     }
 
     public static void homePage(Context ctx, ConnectionPool connectionPool) {
-        //Function made by Nicolai
         //Renders the html Homepage.html and uses the giveCupcakeOptionsToHTML function to give the dropdown bars options
         cupcakeController.giveCupcakeTopOptionsToHTML(connectionPool, ctx);
         cupcakeController.giveCupcakeBottomOptionsToHTML(connectionPool, ctx);
